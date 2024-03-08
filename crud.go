138618a1be1680003b559cf78e42e2ea84ee9781
Ismail BOUMEHDI <ismail.boumehdi@kleegroup.com>
2024-03-08 13:46:16 +0100

    working

--- crud.go
+++ crud.go
@@ -6,13 +6,17 @@ func create(db *gorm.DB, item Todo) {
 	db.Create(&item)
 }
 
-func delete(db *gorm.DB, id int) {
-	db.Delete(&Todo{}, id)
+// func delete(db *gorm.DB, id int) {
+// 	db.Delete(&Todo{}, id)
+// }
+
+func update(db *gorm.DB, item Todo) {
+	db.Save(item)
 }
 
 func getAll(db *gorm.DB) []Todo {
 	var results []Todo
-	db.Find(&results)
+	db.Order("id asc").Find(&results)
 	return results
 }
 
--- main.go
+++ main.go
@@ -3,31 +3,76 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"gorm.io/gorm"
 )
 
-// func getFlag(inputs []string) string {
-// 	flags := []string{}
-// 	for _, i := range inputs {
+func isKeyWork(word string) bool {
+	keywords := []string{"add", "delete", "done", "-d"}
+	for _, i := range keywords {
+		if word == i {
+			return true
+		}
+	}
+	return false
+}
+
+func getKeywordsMap(words []string) map[string]int {
+	res := map[string]int{"add": -1, "delete": -1, "done": -1, "-d": -1}
+	for i, v := range words {
+		if isKeyWork(v) {
+			res[v] = i
+		}
+	}
+	return res
+}
+
+// func getFlag(inputs []string) (string, int) {
+// 	for ind, i := range inputs {
 // 		if strings.Contains(i, "-") {
-// 			flags = append(flags, i)
+// 			return i, ind
 // 		}
 // 	}
-// 	if len(flags) > 0 {
-// 		return flags[0]
-// 	}
-// 	return ""
+// 	return "", -1
 // }
 
 func processInput(inputs []string, db *gorm.DB) {
-
+	keywordsMap := getKeywordsMap(inputs)
 	switch inputs[1] {
 	case "add":
-		create(db, Todo{Title: inputs[2], Description: &inputs[2]})
-
+		title := strings.Join(inputs[2:], " ")
+		desc := ""
+		if keywordsMap["-d"] > -1 {
+			desc = strings.Join(inputs[keywordsMap["-d"]+1:], " ")
+			title = strings.Join(inputs[2:keywordsMap["-d"]], " ")
+		}
+		create(db, Todo{Title: title, Description: &desc})
+	case "done":
+		id, err := strconv.Atoi(inputs[2])
+		if err != nil {
+			return
+		}
+		item := get(db, id)
+		item.IsCompleted = !item.IsCompleted
+		update(db, item)
 	}
 
+}
+
+func displayItems(todos []Todo) {
+	for _, i := range todos {
+		done := "TODO"
+		if i.IsCompleted {
+			done = "DONE"
+		}
+		if *i.Description != "" {
+			fmt.Printf("%d) %s (%s) - %s\n", i.ID, i.Title, *i.Description, done)
+		} else {
+			fmt.Printf("%d) %s - %s\n", i.ID, i.Title, done)
+		}
+	}
 }
 
 func main() {
@@ -41,16 +86,10 @@ func main() {
 	}
 	if len(os.Args) > 2 {
 		processInput(os.Args, db)
-	} else if len(os.Args) < 2 {
-		todos := getAll(db)
-		for _, i := range todos {
-			done := ""
-			if i.IsCompleted {
-				done = "DONE"
-			}
-			fmt.Printf("%d) %s - %s", i.ID, i.Title, done)
-		}
-	} else {
+	} else if len(os.Args) == 2 {
 		panic("Wrong number arguments")
+
 	}
+	todos := getAll(db)
+	displayItems(todos)
 }
